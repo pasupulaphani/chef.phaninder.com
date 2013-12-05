d437dce9f40da2d630ebb9986c7648500ed1d21a
pasupulaphani <pasupulaphani@gmail.com>
2013-12-05 02:37:55 +0000

    using nginx repo to get the latest stable version

--- roles/webserver.rb
+++ roles/webserver.rb
@@ -3,6 +3,8 @@ description "reverse proxy for my app"
 run_list "recipe[nginx]"
 override_attributes({
   :nginx => {
+  	:version => "1.4.4",
+  	:repo_source => "nginx",
     :status => { :port => "8090" }
   }
 })
